43424646fa281e64c79ae4c812d7b7cdc881195f
Timothy Smith <zp7019rc@go.minnstate.edu>
2023-01-25 17:47:26 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int customerNumber = 101;
+        String firstName = "John";
+        String lastName = "Doe";
+        int thermometerNumber = 1001;
+        double temperatureReading = 70.0;
+
+        System.out.println("Customer: " + customerNumber + " " + firstName + " " + lastName);
+        System.out.println("Thermometer #: " + thermometerNumber);
+        System.out.println("Average Temperature: " + temperatureReading);
+    }
+}
